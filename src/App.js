a7b0f8156a7dd62950313d87b5aa4b1b7d9ce2d0
Natalie Tam <30819550+nataliet57@users.noreply.github.com>
2022-03-03 14:22:47 -0500

    reVMOED HOok

--- src/App.js
+++ src/App.js
@@ -28,69 +28,67 @@ class App extends React.Component {
   render() {
     // TODO: I've implemented two ciphers, but this web app
     // doesn't seem to do anything. Please help...
-    useEffect(() => {
-      return (
-        <div>
-          <h1>Cipher Wizard</h1>
-          <div style={{ display: "flex", flexDirection: "row" }}>
-            <select
-              id="input"
-              value={this.state.language}
-              onChange={(e) => this.setState({ language: e.target.value })}
-              style={{ width: "15%" }}
-            >
-              <option value="rot13">rot13</option>
-              <option value="rotN">rotN</option>
-              <option value="base64">base64</option>
-            </select>
-            <div id="output"></div>
-            <button
-              onClick={this.handleUpdateLanguage}
-              style={{
-                height: "20px",
-                marginLeft: "27%",
-                transform: "translate(-50%, 0%)",
-              }}
-            >
-              Switch
-            </button>
-          </div>
-          <br />
-          <label>
-            {"Shift: "}
-            <input
-              value={this.state.shiftValue}
-              onChange={(event) =>
-                this.setState({ shiftValue: event.target.value })
-              }
-              style={{ width: "5%" }}
-            />
-          </label>
-          <br />
-          <br />
-          <div style={{ display: "flex", flexDirection: "row" }}>
-            <textarea
-              value={this.state.textValue}
-              onChange={(event) =>
-                this.setState({ textValue: event.target.value })
-              }
-              style={{ height: "200px", width: "40%" }}
-            />
-            <textarea
-              // value={this.useLanguage(this.language, this.state.textValue)}
-              value={rot13({ text: this.state.textValue })}
-              readOnly
-              style={{
-                height: "200px",
-                width: "40%",
-                marginLeft: "2%",
-                backgroundColor: "powderblue",
-              }}
-            />
-          </div>
+    return (
+      <div>
+        <h1>Cipher Wizard</h1>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <select
+            id="input"
+            value={this.state.language}
+            onChange={(e) => this.setState({ language: e.target.value })}
+            style={{ width: "15%" }}
+          >
+            <option value="rot13">rot13</option>
+            <option value="rotN">rotN</option>
+            <option value="base64">base64</option>
+          </select>
+          <div id="output"></div>
+          <button
+            onClick={this.handleUpdateLanguage}
+            style={{
+              height: "20px",
+              marginLeft: "27%",
+              transform: "translate(-50%, 0%)",
+            }}
+          >
+            Switch
+          </button>
         </div>
-      );
-    });
+        <br />
+        <label>
+          {"Shift: "}
+          <input
+            value={this.state.shiftValue}
+            onChange={(event) =>
+              this.setState({ shiftValue: event.target.value })
+            }
+            style={{ width: "5%" }}
+          />
+        </label>
+        <br />
+        <br />
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <textarea
+            value={this.state.textValue}
+            onChange={(event) =>
+              this.setState({ textValue: event.target.value })
+            }
+            style={{ height: "200px", width: "40%" }}
+          />
+          <textarea
+            // value={this.useLanguage(this.language, this.state.textValue)}
+            value={rot13({ text: this.state.textValue })}
+            readOnly
+            style={{
+              height: "200px",
+              width: "40%",
+              marginLeft: "2%",
+              backgroundColor: "powderblue",
+            }}
+          />
+        </div>
+      </div>
+    );
   }
 }
 
